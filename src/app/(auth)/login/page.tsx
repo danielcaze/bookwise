1cd3a67fe855023e995c3afcaea407e46ea30e63
Daniel Cazé <danielctlima@gmail.com>
2023-07-26 14:07:08 -0300

    :lipstick: fix: mobile-first

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -4,8 +4,8 @@ import Image from "next/image";
 
 export default function Login() {
   return (
-    <div className="h-screen bg-gray800 p-5 flex gap-5 items-center max-md:flex-col max-md:justify-center">
-      <div className="relative md:max-w-[598px] w-full md:h-full rounded-lg overflow-hidden">
+    <div className="h-screen bg-gray800 p-5 flex gap-5 items-center justify-center flex-col md:flex-row md:justify-start">
+      <div className="relative w-full rounded-lg overflow-hidden md:max-w-[598px] md:h-full">
         <Image
           className="max-md:hidden"
           alt="Blonde woman laid on the couch reading a book with Bookwise's logo on the middle"
@@ -15,7 +15,7 @@ export default function Login() {
         />
 
         <Image
-          className="max-md:mx-auto md:absolute md:top-2/4 md:left-2/4 md:-translate-x-2/4 md:-translate-y-2/4"
+          className="max-md:w-[18rem] max-md:mx-auto md:absolute md:top-2/4 md:left-2/4 md:-translate-x-2/4 md:-translate-y-2/4"
           alt=""
           src="/assets/logo.svg"
           width={232}
@@ -23,15 +23,15 @@ export default function Login() {
         />
       </div>
       <div className="w-full">
-        <div className="max-w-[20.438rem] w-full mx-auto flex flex-col gap-10 justify-center items-center justify-self-center align-self-center">
-          <div>
+        <div className="max-w-[18rem] w-full mx-auto flex flex-col gap-10 justify-center items-center justify-self-center align-self-center">
+          <div className="w-full">
             <strong className="text-gray100 text-lg">Boas vindas!</strong>
             <p className="text-gray200 text-md">
               Faça seu login ou acesse como visitante.
             </p>
           </div>
 
-          <div className="flex flex-col gap-4">
+          <div className="w-full flex flex-col gap-4">
             <ButtonLogin provider={PROVIDERS.GOOGLE} />
             <ButtonLogin provider={PROVIDERS.GITHUB} />
             <ButtonLogin />
