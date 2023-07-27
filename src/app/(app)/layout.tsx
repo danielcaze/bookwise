4370bd587c0a60ee0f4e674a97edb8f240c3150f
Daniel Cazé <danielctlima@gmail.com>
2023-07-27 03:39:02 -0300

    :hammer: feat: rename routes

--- src/app/(app)/layout.tsx
+++ src/app/(app)/layout.tsx
@@ -1,5 +1,5 @@
-import { Navbar } from "@/src/components/Navbar";
 import { ReactNode } from "react";
+import Navbar from "@/src/components/Navbar";
 
 export default function AppLayout({ children }: { children: ReactNode }) {
   return (
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -2,7 +2,7 @@ import Image from "next/image";
 import NavigationList from "./NavigationList";
 import LoginStatus from "./LoginStatus";
 
-export function Navbar() {
+export default function Navbar() {
   return (
     <nav className="fixed top-5 bottom-5 left-5 bg-gradient-vertical-opaque bg-gray700 h-[calc(100%_-_2.5rem)] w-[14.5rem] rounded-2xl flex flex-col justify-between pt-10 pb-6 px-14">
       <div className="flex flex-col gap-16">
