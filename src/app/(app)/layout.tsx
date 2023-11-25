c0aa0d90f5f4eb90d180da86864c3dbb6e978b11
Daniel Cazé <danielctlima@gmail.com>
2023-11-24 22:29:44 -0300

    :bug: fix: scroll

--- src/app/(app)/layout.tsx
+++ src/app/(app)/layout.tsx
@@ -3,12 +3,12 @@ import Navbar from "@/src/components/Navbar";
 
 export default function AppLayout({ children }: { children: ReactNode }) {
   return (
-    <div className="grid grid-cols-[calc(14.5rem_+_1.25rem)_1fr] gap-24 bg-gray800 min-h-screen">
+    <div className="grid grid-cols-[calc(14.5rem_+_1.25rem)_1fr] gap-24 min-h-screen">
       <div className="relative">
         <Navbar />
       </div>
 
-      <div className="mt-[4.5rem] mr-24 max-w-[calc(100vw_-_14.5rem_-_1.25rem_-_6rem)] overflow-x-hidden">
+      <div className="mt-[4.5rem] mr-24 max-w-[calc(100vw_-_14.5rem_-_1.25rem_-_6rem)]">
         {children}
       </div>
     </div>
